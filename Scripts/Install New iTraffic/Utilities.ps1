89c3ae133d6f62abd32f0271f4ec552e2d39fabd
Alejandro Sanchez <sanchezalejandroismael@gmail.com>
2026-02-06 14:24:49 -0300

    Update Utilities.ps1 to specify the winget source for installations, enhancing clarity and ensuring consistent package retrieval.

--- Scripts/Install New iTraffic/Utilities.ps1
+++ Scripts/Install New iTraffic/Utilities.ps1
@@ -2,6 +2,6 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     Write-Host "Winget no está disponible." -ForegroundColor Red
     exit
 }
-winget install --id 7zip.7zip --silent --accept-package-agreements --accept-source-agreements
-winget install --id Notepad++.Notepad++ --silent --accept-package-agreements --accept-source-agreements
-winget install --id Microsoft.SQLServerManagementStudio --silent --accept-package-agreements --accept-source-agreements
+winget install --id 7zip.7zip --source winget --silent --accept-package-agreements --accept-source-agreements
+winget install --id Notepad++.Notepad++ --source winget --silent --accept-package-agreements --accept-source-agreements
+winget install --id Microsoft.SQLServerManagementStudio --source winget --silent --accept-package-agreements --accept-source-agreements
